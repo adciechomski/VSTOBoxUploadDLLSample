37a123fcc21539f5b7c11ad15a1745848d8cc783
aciec <aciec@DESKTOP-4JQ7ALO>
2017-12-28 22:51:06 -0700

    Add project files.

---
+++ BoxUpload/BoxCalls.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using System.Runtime.InteropServices;
+using RestSharp;
+
+namespace BoxUpload
+{
+    [ComVisible(true)]
+    public interface IBoxCalls
+    {
+        void Upload_Doc(string folderId, string accessToken, string filePath, string fileName);
+    }
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class BoxCalls : IBoxCalls
+    {
+        public void Upload_Doc(string folderId, string accessToken, string filePath, string fileName)
+        {
+            var client = new RestClient("https://upload.box.com/api/2.0");
+            var request = new RestRequest("files/content", Method.POST);
+            request.AddParameter("parent_id", folderId);
+            request.AddHeader("Authorization", "Bearer " + accessToken);
+            string path = filePath + fileName;
+            byte[] byteArray = System.IO.File.ReadAllBytes(path);
+            request.AddFile("filename", byteArray, fileName);
+            var responses = client.Execute(request);
+            var content = responses.Content;
+        }
+    }
+}
---
+++ BoxUpload/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using System.Runtime.InteropServices;
+using System.Data;
+
+namespace BoxUpload
+{
+    public partial class Main
+    {
+        private BoxCalls boxCallsCollection;
+
+    protected override object RequestComAddInAutomationService()
+    {
+        if (boxCallsCollection == null)
+                boxCallsCollection = new BoxCalls();
+        return boxCallsCollection;
+    }
+    #region VSTO generated code
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }        
+        #endregion
+    }
+}
